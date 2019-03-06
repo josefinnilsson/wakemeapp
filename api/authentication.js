f756ed9ad023d088fcc1673f42828c321c85866a
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-03-06 18:55:37 +0100

    Add functionality that verifies a token

---
+++ api/authentication.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+const authenticate = function(req, res, next) {
+    const token = req.body.token || req.query.token ||
+                  req.headers['x-access-token'] || req.cookies.token
+    if (!token) {
+        res.status(401).send('Unauthorized: No token provided')
+    } else {
+        jwt.verify(token, secret, (err, decoded) => {
+            if (err) {
+                res.status(401).send('Unauthorized: Invalid token')
+            } else {
+                req.email = decoded.email
+                next()
+            }
+        })
+    }
+}
+
+module.exports = authenticate
