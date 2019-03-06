a1862fe9d8afbb186237f6d0502dc82b9cccc195
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-03-06 17:24:04 +0100

    Install dependencies

---
+++ api/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const salt_rounds = 10
+
+const UserSchema = new mongoose.Schema({
+    name:       { type: String, required: true },
+    email:      { type: String, required: true, unique: true },
+    password:   { type: String, required: true }
+})
+
+UserSchema.pre('save', function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const document = this
+        bcrypt.hash(document.password, salt_rounds, function(err, hashed_password) {
+            console.log(hashed_password)
+            if (err) {
+                console.log("err")
+                next(err)
+            } else {
+                document.password = hashed_password
+                next()
+            }
+        })
+    } else {
+        next()
+    }
+})
+
+UserSchema.methods.checkPassword = (password, callback) => {
+    bcrypt.compare(password, this.password, (err, same) => {
+        if (err) {
+            callback(err)
+        } else {
+            callback(err, same)
+        }
+    })
+}
+
+module.exports = mongoose.model('User', UserSchema)
