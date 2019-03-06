328391b453a9d8d29c33340360fc9f176198c4bb
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-03-06 20:41:51 +0100

    Fix class structure

--- client/src/authenticated.js
+++ client/src/authenticated.js
@@ -1,40 +1,42 @@
 import React, {Component} from 'react'
 import {Redirect} from 'react-router-dom'
 
-class Authenticated extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            loading: true,
-            should_login: false,
+export default function Authenticated(Component) {
+    return class extends Component {
+        constructor(props) {
+            super(props)
+            this.state = {
+                loading: true,
+                should_login: false,
+            }
+        }
+
+        componentDidMount() {
+            fetch('/validToken')
+            .then(res => {
+                if (res.status === 200) {
+                    this.setState({ loading: false })
+                } else {
+                    throw new Error(res.error)
+                }
+            }).catch(err => {
+                console.error(err)
+                this.setState({ loading: false, should_login: true})
+            })
+        }
+
+        render() {
+            if (this.state.loading)
+                return null
+            if (this.state.should_login)
+                return <Redirect to="/login"/>
+            else {
+                return (
+                    <React.Fragment>
+                    <Component {...this.props} />
+                    </React.Fragment>
+                )
+            }
         }
     }
-
-    componentDidMount() {
-        fetch('/validToken')
-        .then(res => {
-            if (res.status === 200) {
-                this.setState({ loading: false })
-            } else {
-                throw new Error(res.error)
-            }
-        }).catch(err => {
-            console.error(err)
-            this.setState({ loading: false, should_login: true})
-        })
-    }
-
-    render() {
-        if (this.state.loading)
-            return null
-        if (this.stateshould_login)
-            return <Redirect to="/login"/>
-        return (
-            <React.Fragment>
-                <Component {...this.props} />
-            </React.Fragment>
-        )
-    }
 }
-
-export default Authenticated
