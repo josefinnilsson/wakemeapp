0ce60ff1d07b0d6dd38a86f6f36d40ffaf3df69b
Helena Alinder <helenaalinder@gmail.com>
2019-03-08 16:38:36 +0100

    fetch weather data based on current location

--- api/server.js
+++ api/server.js
@@ -33,7 +33,7 @@ router.get('/test', function(req, res, next) {
 /*
 A promise that gets station information when the user search for a station
 */
-router.get('/getStationData/:search_string', function(req, res, next) {
+router.get('/getStationData/:search_string', (req, res, next) => {
 	let api_key = process.env.PLATSUPPSLAG
     let search_string = req.params.search_string
     const options = {
@@ -53,7 +53,7 @@ router.get('/getStationData/:search_string', function(req, res, next) {
 /*
 A promise that gets real time information about a station's departures
 */
-router.get('/getRealTime/:station_id/:bus/:metro/:train/:tram/:ship', function(req, res, next) {
+router.get('/getRealTime/:station_id/:bus/:metro/:train/:tram/:ship', (req, res, next) => {
     let api_key = process.env.REALTIME
     let station_id = parseInt(req.params.station_id)
 
@@ -69,6 +69,48 @@ router.get('/getRealTime/:station_id/:bus/:metro/:train/:tram/:ship', function(r
         })
     }).then(body => {
         res.json(JSON.stringify(JSON.parse(body).ResponseData))
+    }).catch(error => {
+        console.log(error)
+    })
+})
+
+/*
+A promise of weather data for the current location of the client's browser
+*/
+router.get('/weather/:latitude/:longitude', (req, res, next) => {
+    let api_key = process.env.WEATHER
+    const options = {
+        url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + req.params.latitude
+        + '&lon=' + req.params.longitude + '&units=metric' + '&appid=' + api_key
+    }
+
+    return new Promise(resolve => {
+        request(options, (err, res, body) => {
+            resolve(body)
+        })
+    }).then(body => {
+        let data = JSON.parse(body)
+        let id = data.weather[0].id.toString()
+        let first_char = id.charAt(0)
+
+        // add icons for each type of weather here
+        if (first_char === '2') {
+            data.icon = 'path to thunder icon'
+        } else if (first_char === '3') {
+            data.icon = 'path to drizzle icon'
+        } else if (first_char === '5') {
+            data.icon = 'path to rain icon'
+        } else if (first_char === '6') {
+            data.icon = 'path to snow icon'
+        } else if (first_char === '8') {
+            let third_char = id.charAt(2)
+            if (third_char === '0') {
+                data.icon = 'path to sun icon'
+            } else {
+                data.icon = 'path to cloud icon'
+            }
+        }
+        res.json(data)
     }).catch(error => {
         console.log(error)
     })
