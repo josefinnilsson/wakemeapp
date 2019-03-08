4f72eefdfd05f9505b06d745f179ed2f44fb6ee6
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-03-08 14:35:40 +0100

    Use redux store

---
+++ client/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers'
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly'
+
+const initialState = {}
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(
+        applyMiddleware(thunk)
+    )
+)
+
+export default store
