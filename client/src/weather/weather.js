b3dbacc77379f0c889525ee255f555d7c8023094
Helena Alinder <helenaalinder@gmail.com>
2019-03-10 16:41:00 +0100

    fetch and display weather data

--- client/src/weather/weather.js
+++ client/src/weather/weather.js
@@ -2,11 +2,69 @@ import React, { Component } from 'react';
 import './weather.css';
 
 class Weather extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      status: 'INIT',
+      latitude: 0,
+      longitude: 0
+    }
+    this.handleRefresh = this.handleRefresh.bind(this)
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        })
+      }, error => {console.log(error)})
+  }
+
+  handleRefresh() {
+    let lat = this.state.latitude
+    let long = this.state.longitude
+
+    fetch('/weather/' + lat + '/' + long)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        localStorage.setItem('has_weather_details', true)
+        localStorage.setItem('weather', JSON.stringify(data))
+        this.setState({
+          status: 'LOADED',
+          weather: data
+        })
+      })
+  }
 
   render() {
+    const RefreshWeather = () => {
+      return (<button id='refresh_weather' onClick={this.handleRefresh}>Refresh weather</button>)
+    }
+    if (localStorage.getItem('weather') === null)
+      this.handleRefresh()
+    let weather = ''
+    if (localStorage.getItem('has_weather_details'))
+      weather = JSON.parse(localStorage.getItem('weather'))
+    // temp check for when data doesn't exist
+    if (weather === '' || weather === null)
+      return (<div><RefreshWeather/></div>)
+
     return (
       <div>
-
+        <RefreshWeather/>
+        <div>
+          {/*
+            How to get weather icons when they exist
+            <img src={weather.icon} alt='weather icon' height='25' width='25'></img>
+          */}
+          <p>{weather.name + ': ' }</p>
+          <p>{weather.weather[0].description}</p>
+          <p>{weather.main.temp + ' \u00b0C'}</p>
+        </div>
       </div>
     )
   }
