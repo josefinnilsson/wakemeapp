cbbbe36619256a8af6a9602800e09ef3667cc899
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-03-13 18:59:07 +0100

    Continue working with calendar auth logic

--- api/calendar.js
+++ api/calendar.js
@@ -4,45 +4,46 @@ const {google} = require('googleapis')
 const fs = require('fs')
 const oAuth = google.auth.OAuth2
 const TOKEN_PATH = 'token.json'
+const oAuth2Client = new oAuth(process.env.CLIENT_ID, process.env.CLIENT_SECRET, 'http://localhost:3001/calendar_callback')
 
 const CalendarAPI = {
-    authorize: function(callback) {
-        const oAuth2Client = new oAuth(process.env.CLIENT_ID, process.env.CLIENT_SECRET, 'http://localhost:3001')
-        fs.readFile(TOKEN_PATH, (err, token) => {
-            if (err)
-                return CalendarAPI.getAccessToken(oAuth2Client, callback)
-            oAuth2Client.setCredentials(JSON.parse(token))
-            callback(oAuth2Client)
-        })
-    },
-    getAccessToken: function(oAuth2Client, callback) {
-        const authUrl = oAuth2Client.generateAuthUrl({
-            access_type: 'offline', scope: 'https://www.googleapis.com/auth/calendar.readonly'
-        })
-        console.log('Authorize by visiting: ', authUrl)
-        const input = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        })
-        input.question('Enter the code: ', (code) => {
-            input.close()
-            oAuth2Client.getToken(code, (err, token) => {
-                if (err)
-                    console.log('Error retrieving access token')
-                else {
-                    console.log('Authentication success')
+    authorize: function(res) {
+        return new Promise(resolve => {
+            fs.readFile(TOKEN_PATH, (err, token) => {
+                if (err) {
+                    CalendarAPI.getAccessToken(res)
+                    resolve('NO_TOKEN')
+                } else {
+                    oAuth2Client.setCredentials(JSON.parse(token))
+                    resolve('TOKEN')
                 }
-                oAuth2Client.setCredentials(token)
-                fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                    if (err)
-                        console.log(err)
-                })
-                callback(oAuth2Client)
             })
         })
     },
-    listEvents: function(auth) {
-        const calendar = google.calendar({ version: 'v3', auth })
+    getAccessToken: function(res) {
+        const authUrl = oAuth2Client.generateAuthUrl({
+            access_type: 'offline', scope: 'https://www.googleapis.com/auth/calendar.readonly'
+        })
+        res.send(authUrl)
+    },
+    createToken: function(code) {
+        return new Promise((resolve, reject) => {
+            oAuth2Client.getToken(code, (err, token) => {
+                if (err)
+                    reject('Error retrieving access token')
+                else {
+                    oAuth2Client.setCredentials(token)
+                    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+                        if (err)
+                            reject('Token err')
+                    })
+                    resolve('Created token sucessfully')
+                }
+            })
+        })
+    },
+    listEvents: function() {
+        const calendar = google.calendar('v3')
         calendar.events.list({
             calendarId: 'primary',
             timeMin: (new Date()).toISOString(),
@@ -64,40 +65,6 @@ const CalendarAPI = {
             }
         })
     }
-}
-exports.authorize = (credentials, callback) => {
-    const oAuth2Client = new oAuth(process.env.CLIENT_ID, process.env.CLIENT_SECRET, 'https://localhost:3000')
-
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err)
-            return getAccessToken(oAuth2Client, callback)
-        oAuth2Client.setCredentials(JSON.parse(token))
-        callback(oAuth2Client)
-    })
-}
-
-exports.getAccessToken = (oAuth2Client, callback) => {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline', scope: 'https://www.googleapis.com/auth/calendar.readonly'
-    })
-    console.log('Authorize by visiting: ', authUrl)
-    const input = readline.creaeInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
-    input.question('Enter the code: ', (code) => {
-        input.close()
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err)
-                conosle.log('Error retrieving access token')
-            oAuth2Client.setCredentials(token)
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err)
-                    console.log(err)
-            })
-            callback(oAuth2Client)
-        })
-    })
 }
 
 module.exports = CalendarAPI
