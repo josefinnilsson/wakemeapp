1a055ded653f37c7b644d3f45c1e87f87f232409
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-03-14 17:05:40 +0100

    Start working with calendar frontend

--- client/src/calendar/calendar.js
+++ client/src/calendar/calendar.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import './calendar.css';
 
 class Calendar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            status: 'INIT',
+            events: [],
+        }
+        this.handleRefresh = this.handleRefresh.bind(this)
+    }
 
-  render() {
+    handleRefresh() {
+      fetch('/calendar')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        if (data.url) {
+          window.open(data.url, '_blank')
+        } else {
+          this.setState({
+            status: 'LOADED',
+            events: JSON.stringify(data)
+          })
+        }
+      })
+    }
+
+    render() {
+    const RefreshCalendar = () => {
+      return (<button id='refresh_calendar' onClick={this.handleRefresh}>Refresh calendar</button>)
+    }
+
     return (
       <div>
-
+        <RefreshCalendar/>
+        <div>
+          <p>{this.state.events}</p>
+          <p>{this.state.events}</p>
+        </div>
       </div>
     )
   }
