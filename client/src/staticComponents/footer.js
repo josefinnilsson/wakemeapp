00c5fe86cfad6d366ddd850d5de38c41d8ac3496
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-03-19 18:04:26 +0100

    Create footer component

---
+++ client/src/staticComponents/footer.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import './staticComponents.scss'
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer class="page-footer font-small teal pt-4">
+        <div class="container-fluid text-center text-md-left">
+          <div class="row">
+            <div class="col-md-6 mt-md-0 mt-3">
+              <h5>About</h5>
+              <p>Wake Me App was developed by Helena Alinder & Josefin Nilsson as project for DH2642 Interaction Programming and the Dynamic Web.</p>
+            </div>
+            <hr class="clearfix w-100 d-md-none pb-3"/>
+            <div class="col-md-6 mb-md-0 mb-3">
+              <h5>Acknowledgements</h5>
+              <p>The application uses the following APIs:&nbsp;
+              <a href="https://developers.google.com/calendar/">Google Calendar</a>,&nbsp;
+              <a href="https://developers.google.com/maps/documentation/geocoding/start">Google Geocoding</a>,&nbsp;
+              <a href="https://api.nasa.gov/">NASA</a>,&nbsp;
+              <a href="https://newsapi.org/s/svenska-dagbladet-api">News API</a>,&nbsp;
+              <a href="https://openweathermap.org/api">Open Weather Map</a>,&nbsp;
+              <a href="https://www.trafiklab.se/">Trafiklab</a>,&nbsp;
+              <a href="https://source.unsplash.com/">Unsplash</a>.</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
+ }
+}
+
+export default Footer
