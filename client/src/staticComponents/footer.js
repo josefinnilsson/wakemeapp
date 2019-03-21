baae894ba79b39052853bd9900181891cd5f0072
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-03-21 17:52:56 +0100

    Fix footer styling

--- client/src/staticComponents/footer.js
+++ client/src/staticComponents/footer.js
@@ -4,17 +4,17 @@ import './staticComponents.scss'
 class Footer extends Component {
   render() {
     return (
-      <footer className="page-footer font-small teal pt-4">
+      <div className="page-footer font-small teal pt-4 fixed-bottom">
         <div className="container-fluid text-center text-md-left">
           <div className="row">
             <div className="col-md-6 mt-md-0 mt-3">
               <h5>About</h5>
-              <p>Wake Me App was developed by Helena Alinder & Josefin Nilsson as project for DH2642 Interaction Programming and the Dynamic Web.</p>
+              <p className="small">Wake Me App was developed by Helena Alinder & Josefin Nilsson as project for DH2642 Interaction Programming and the Dynamic Web.</p>
             </div>
             <hr className="clearfix w-100 d-md-none pb-3"/>
             <div className="col-md-6 mb-md-0 mb-3">
               <h5>Acknowledgements</h5>
-              <p>The application uses the following APIs:&nbsp;
+              <p className="small">The application uses the following APIs:&nbsp;
               <a href="https://developers.google.com/calendar/">Google Calendar</a>,&nbsp;
               <a href="https://developers.google.com/maps/documentation/geocoding/start">Google Geocoding</a>,&nbsp;
               <a href="https://api.nasa.gov/">NASA</a>,&nbsp;
@@ -25,7 +25,7 @@ class Footer extends Component {
             </div>
           </div>
         </div>
-      </footer>
+      </div>
     )
  }
 }
