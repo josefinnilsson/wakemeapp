66c91126b49930141271b8f9bc710f7e00b40a6e
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-03-27 16:12:44 +0100

    Start working with settings page styling

--- client/src/settings/userSettings.js
+++ client/src/settings/userSettings.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './userSettings.scss'
-import Dropdown from 'react-bootstrap/Dropdown'
 import axios from 'axios'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { logout } from '../actions/authActions'
 import BackgroundToggle from '../settings/backgroundToggle'
+import {Form, Button} from 'react-bootstrap'
 
 const mapStateToProps = state => ({
   auth: state.auth
@@ -25,12 +25,19 @@ class UserSettings extends Component {
       metro: false,
       train: false,
       tram: false,
-      ship: false
+      ship: false,
+      search: '',
     }
 
     this.handleSaveSubmit = this.handleSaveSubmit.bind(this)
     this.handleSearchSubmit = this.handleSearchSubmit.bind(this)
     this.handleStationChange = this.handleStationChange.bind(this)
+    this.handleSearchChange = this.handleSearchChange.bind(this)
+    this.handleBusChange = this.handleBusChange.bind(this)
+    this.handleMetroChange = this.handleMetroChange.bind(this)
+    this.handleTrainChange = this.handleTrainChange.bind(this)
+    this.handleShipChange = this.handleShipChange.bind(this)
+    this.handleTramChange = this.handleTramChange.bind(this)
   }
 
   componentDidMount() {
@@ -46,14 +53,14 @@ class UserSettings extends Component {
 
   handleSearchSubmit(e) {
     e.preventDefault()
-    fetch('/getStationData/' + this.refs.search_string.value)
+    fetch('/getStationData/' + this.state.search)
       .then(response => {
         return response.json()
       })
       .then(stations => {
         let dropdown_items = []
         for (let i = 0; i < stations.length; i++) {
-          dropdown_items.push(<Dropdown.Item key={stations[i].SiteId + i} onClick={(name, id) => this.handleStationChange(stations[i].Name, stations[i].SiteId)}>{stations[i].Name}</Dropdown.Item>)
+          dropdown_items.push(<h6 className="list_item" key={stations[i].SiteId + i} onClick={(name, id) => this.handleStationChange(stations[i].Name, stations[i].SiteId)}>{stations[i].Name}</h6>)
         }
         this.setState({
           stations: dropdown_items
@@ -69,11 +76,11 @@ class UserSettings extends Component {
     let stationId = this.state.active_station_id
     if (this.state.active_station_id === 0)
       stationId = localStorage.getItem('user_station_id')
-    const bus = this.refs.bus.checked
-    const metro = this.refs.metro.checked
-    const train = this.refs.train.checked
-    const tram = this.refs.tram.checked
-    const ship = this.refs.ship.checked
+    const bus = this.state.bus
+    const metro = this.state.metro
+    const train = this.state.train
+    const tram = this.state.tram
+    const ship = this.state.ship
     const userSettings = {
       stationName: stationName,
       stationId: stationId,
@@ -110,6 +117,30 @@ class UserSettings extends Component {
     })
   }
 
+  handleSearchChange(e) {
+    this.setState({ search: e.target.value })
+  }
+
+  handleBusChange(e) {
+    this.setState({ bus: !this.state.bus })
+  }
+
+  handleMetroChange(e) {
+    this.setState({ metro: !this.state.metro })
+  }
+
+  handleTrainChange(e) {
+    this.setState({ train: !this.state.train })
+  }
+
+  handleTramChange(e) {
+    this.setState({ tram: !this.state.tram })
+  }
+
+  handleShipChange(e) {
+    this.setState({ ship: !this.state.ship })
+  }
+
   onLogout = e => {
     e.preventDefault()
     this.props.logout()
@@ -118,48 +149,38 @@ class UserSettings extends Component {
 
   render() {
     const stations = this.state.stations
-    const StationDropdown = () => (
-        <div>
-          <Dropdown>
-                <Dropdown.Toggle id="dropdown-basic">
-                    Stations
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    {stations}
-                </Dropdown.Menu>
-            </Dropdown>
-        </div>
-      )
 
     return (
-      <div className='container'>
-        <div>
-          <button onClick={this.onLogout}>Logout</button>
-          <Link to='/'><button>Back to dashboard</button></Link>
-          <BackgroundToggle/>
-          <div>
-            <h2>Settings</h2>
-            <p>Current station: {this.state.active_station !== '' ? this.state.active_station : ' - '}</p>
-            <form onSubmit={this.handleSearchSubmit}>
-              <label>Search station</label><input type='text' ref='search_string' minLength='3'/>
-              <input type='submit' value='search'/>
-            </form>
-            {localStorage.getItem('user_station_id') === '-1' ? (stations.length > 0 ? <StationDropdown/> : '') : stations.length > 0 ? <StationDropdown/> : ''}
-            <form onSubmit={this.handleSaveSubmit}>
-              <label>Bus</label>
-              <input type='checkbox' value='bus' ref='bus' defaultChecked={localStorage.getItem('user_bus') === 'true'}/><br/>
-              <label>Metro</label>
-              <input type='checkbox' value='metro' ref='metro' defaultChecked={localStorage.getItem('user_metro') === 'true'}/><br/>
-              <label>Train</label>
-              <input type='checkbox' value='train' ref='train' defaultChecked={localStorage.getItem('user_train') === 'true'}/><br/>
-              <label>Tram</label>
-              <input type='checkbox' value='tram' ref='tram' defaultChecked={localStorage.getItem('user_tram') === 'true'}/><br/>
-              <label>Ship</label>
-              <input type='checkbox' value='ship' ref='ship' defaultChecked={localStorage.getItem('user_ship') === 'true'}/><br/>
-              <input type='submit' value='Save'/>{this.state.user_saved ? (<p>Settings saved!</p>): ''}
-            </form>
+      <div>
+        <h2>Settings</h2>
+        <div className="settings_form_wrapper">
+          <div className="settings_form">
+            <h3>Departures</h3>
+            <h6>Current station: {this.state.active_station !== '' ? this.state.active_station : ' None'}</h6>
+            <Form onSubmit={this.handleSearchSubmit}>
+              <Form.Group controlId="form_search">
+                <Form.Control type="text" placeholder="Station" minLength="3" value={this.state.search} onChange={this.handleSearchChange}/>
+              </Form.Group>
+              <Button variant="primary" type="submit">Search</Button>
+            </Form>
+            {stations}
+            <div className="transportation_options">
+              <Form onSubmit={this.handleSaveSubmit}>
+                <Form.Check inline label="Bus" type={'checkbox'} id={'bus'} onChange={this.handleBusChange}/>
+                <Form.Check inline label="Metro" type={'checkbox'} id={'metro'} onChange={this.handleMetroChange}/>
+                <Form.Check inline label="Train" type={'checkbox'} id={'train'} onChange={this.handleTrainChange}/>
+                <Form.Check inline label="Tram" type={'checkbox'} id={'tram'} onChange={this.handleTramChange}/>
+                <Form.Check inline label="Ship" type={'checkbox'} id={'ship'} onChange={this.handleShipChange}/>
+                <Button variant="primary" type="submit">Save</Button>
+              </Form>
+            </div>
+
+            <h3>Theme</h3>
+            <BackgroundToggle/>
           </div>
         </div>
+        <button onClick={this.onLogout}>Logout</button>
+        <Link to='/'><button>Back to dashboard</button></Link>
       </div>
     )
   }
