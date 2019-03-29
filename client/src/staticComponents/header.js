52f8093fd41c26b4ced82b72f9cebe7c6c96c51c
Helena Alinder <helenaalinder@gmail.com>
2019-03-29 11:38:50 +0100

    not show user settings button on user settings

--- client/src/staticComponents/header.js
+++ client/src/staticComponents/header.js
@@ -4,13 +4,19 @@ import settings from '../assets/settings.svg'
 
 class Header extends Component {
   render() {
+    let user_settings = false
+    console.log(window.location.pathname)
+    if (window.location.pathname === '/userSettings')
+        user_settings = true
+
+    const user_settings_wheel = !user_settings ? (<a className="user_settings_wheel" href="/userSettings"><img src={settings} alt="settings" className="settings"/></a>) : ''
     return (
         <nav className="navbar">
             <div className="container-fluid">
                 <div className="navbar-header">
                     <a className="navbar-brand title" href="/">Wake Me App</a>
                 </div>
-                <a href="/userSettings"><img src={settings} alt="settings" className="settings"/></a>
+                {user_settings_wheel}
             </div>
         </nav>
     )
