b630438c94cc8468a67dc40dfbbd70e752725a8c
Helena Alinder <helenaalinder@gmail.com>
2019-03-29 16:08:21 +0100

    only show special background on dashboard

--- client/src/settings/backgroundToggle.js
+++ client/src/settings/backgroundToggle.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react'
 import Dropdown from 'react-bootstrap/Dropdown'
 
 class BackgroundToggle extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            background: localStorage.getItem('background')
+        }
+    }
     onThemeChange(e) {
         this.setState({ background: e })
-        if (e === 'NONE') {
-            document.body.style.backgroundImage = ""
+        if (e === 'Minimalistic') {
             localStorage.setItem('background_url', '')
+            localStorage.setItem('background', 'Minimalistic')
         } else if (e === 'NASA') {
             fetch('/nasa')
             .then(response => {
@@ -14,7 +20,7 @@ class BackgroundToggle extends Component {
             })
             .then(data => {
                 localStorage.setItem('background_url', JSON.stringify(data))
-                document.body.style.backgroundImage = `url(${data})`
+                localStorage.setItem('background', 'NASA')
             })
         } else {
             fetch('/unsplash')
@@ -24,23 +30,24 @@ class BackgroundToggle extends Component {
             .then(data => {
                 console.log(data)
                 localStorage.setItem('background_url', JSON.stringify(data.url))
-                document.body.style.backgroundImage = `url(${data.url})`
+                localStorage.setItem('background', 'Unsplash')
+
             })
         }
     }
 
     render() {
         return (
-            <div>
+            <div className="background_toggle">
             <Dropdown>
-                <Dropdown.Toggle id="dropdown-basic">
-                    Theme
+                <Dropdown.Toggle className="settings_btn background_btn" id="dropdown-basic">
+                    {this.state.background !== null ? this.state.background : 'Theme'}
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu>
-                    <Dropdown.Item onClick={(e) => this.onThemeChange('NONE')}>Minimalistic</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => this.onThemeChange('Minimalistic')}>Minimalistic</Dropdown.Item>
                     <Dropdown.Item onClick={(e) => this.onThemeChange('NASA')}>NASA</Dropdown.Item>
-                    <Dropdown.Item onClick={(e) => this.onThemeChange('UNSPLASH')}>Unsplash</Dropdown.Item>
+                    <Dropdown.Item onClick={(e) => this.onThemeChange('Unsplash')}>Unsplash</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
             </div>
