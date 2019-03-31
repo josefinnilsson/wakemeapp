3694797f9e50066639f8ec4bd2b66e2a8ca93a17
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-03-31 21:43:00 +0200

    Send additional cal event info

--- api/calendar.js
+++ api/calendar.js
@@ -90,7 +90,11 @@ const CalendarAPI = {
                     let events_json = []
                     events.map((event, i) => {
                         const start = event.start.dateTime || event.start.date
-                        const json = {start: start, summary: event.summary}
+                        const end = event.end.dateTime || event.end.date
+                        const location = event.location || 'NO_LOCATION'
+                        const link = event.htmlLink
+                        console.log(link)
+                        const json = {start: start, end: end, location: location, summary: event.summary, link: link}
                         events_json.push(json)
                     })
                     res.json({events: events_json})
