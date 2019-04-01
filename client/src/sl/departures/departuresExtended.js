8e45154cfdf2ba9d83fc5833fcc950174243484b
Helena Alinder <helenaalinder@gmail.com>
2019-04-01 16:14:52 +0200

    style departures for desktop and mobile view

--- client/src/sl/departures/departuresExtended.js
+++ client/src/sl/departures/departuresExtended.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import {isMobile} from 'react-device-detect'
 import '../sl.scss'
 import Departures from './departures'
+
 
 class DeparturesExtended extends Component {
   render() {
@@ -21,25 +23,59 @@ class DeparturesExtended extends Component {
         destination={departure.Destination} exp_time={departure.DisplayTime}/>)
     }
 
+
+    const RealTime = () => {
+      return (  <div className='departures_extended real_time'>
+                  <table>
+                  <tbody>
+                    {departures}
+                  </tbody>
+                  </table>
+                </div>)
+    }
+
+    const NoRealTime = () => {
+      return (<div className='departures_extended no_real_time'>
+              {no_real_time_departures.length > 0 ? (<div><table>
+                <tbody>
+                {no_real_time_departures}
+                </tbody>
+              </table></div>) : (<p>No information available</p>)}
+              </div>)
+    }
+
+    const MobileView = () => {
+      return (<Tabs>
+                <TabPanel>
+                  <RealTime/>
+                </TabPanel>
+                <TabPanel>
+                  <NoRealTime/>
+                </TabPanel>
+                <TabList>
+                  <Tab>Real Time</Tab>
+                  <Tab>No Real Time</Tab>
+                </TabList>
+              </Tabs>)
+    }
+
+    const DesktopView = () => {
+      return (<div className='row departures_wrapper'>
+            <div className={no_real_time_departures.length > 0 ? 'col-md-6' : 'col-md-12'}>
+              <h2>Real Time</h2>
+              <RealTime/>
+            </div>
+            {no_real_time.length > 0 ? (
+            <div className='col-md-6'>
+              <h2>No Real Time</h2>
+              <NoRealTime/>
+            </div>) : ''}
+          </div>)
+    }
+    console.log(isMobile)
     return (
       <div className='container'>
-        <div>
-          <Link to='/'><button>Back to dashboard</button></Link>
-          <div className='departures_extended'>
-          <h3>Departures</h3>
-          <table>
-            <tbody>
-              {departures}
-            </tbody>
-          </table>
-          <h3>Departures with no real time</h3>
-          <table>
-            <tbody>
-              {no_real_time_departures}
-            </tbody>
-          </table>
-          </div>
-        </div>
+       {isMobile ? <MobileView/> : <DesktopView/>}
       </div>
     )
   }
