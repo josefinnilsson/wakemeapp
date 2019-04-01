0e94da6fc4a852c3477765d9c77c79afbf5e7c75
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-04-01 14:39:37 +0200

    Create extended weather view

---
+++ client/src/weather/weatherExtended.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './weather.scss'
+
+class WeatherExtended extends Component {
+  render() {
+    return (
+      <h2>Weather</h2>
+    )
+  }
+}
+
+export default WeatherExtended
