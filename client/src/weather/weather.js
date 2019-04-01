4636b1ac8f48e42facabf4d88814af4a2ddf5ca6
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-04-01 19:20:05 +0200

    Style weather component

--- client/src/weather/weather.js
+++ client/src/weather/weather.js
@@ -4,7 +4,12 @@ import { Link } from 'react-router-dom'
 import Moment from 'react-moment'
 import sunrise_icon from '../assets/sunrise.png'
 import sunset_icon from '../assets/sunset.png'
-import ReactAnimatedWeather from 'react-animated-weather'
+import cloudy_icon from '../assets/cloudy.svg'
+import drizzle_icon from '../assets/drizzle.svg'
+import rainy_icon from '../assets/rainy.svg'
+import snowy_icon from '../assets/snowy.svg'
+import sunny_icon from '../assets/sunny.svg'
+import thunder_icon from '../assets/thunder.svg'
 
 class Weather extends Component {
   constructor(props) {
@@ -61,6 +66,25 @@ class Weather extends Component {
     return description.charAt(0).toUpperCase() + description.slice(1)
   }
 
+  getIcon(icon) {
+    switch(icon) {
+      case 'CLOUD':
+        return <img src={cloudy_icon} alt="" className="weather_icon"/>
+      case 'DRIZZLE':
+        return <img src={drizzle_icon} alt="" className="weather_icon"/>
+      case 'RAIN':
+        return <img src={rainy_icon} alt="" className="weather_icon"/>
+      case 'SNOW':
+        return <img src={snowy_icon} alt="" className="weather_icon"/>
+      case 'SUN':
+        return <img src={sunny_icon} alt="" className="sunny_icon"/>
+      case 'THUNDER':
+        return <img src={thunder_icon} alt="" className="weather_icon"/>
+      default:
+        break
+    }
+  }
+
   render() {
     const RefreshWeather = () => {
       return (<button id='refresh_weather' onClick={this.handleRefresh}>Refresh weather</button>)
@@ -69,7 +93,7 @@ class Weather extends Component {
     const location = localStorage.getItem('current_location')
     if (localStorage.getItem('has_weather_details') === 'true' && weather !== null) {
       weather = JSON.parse(weather)
-      console.log(weather.icon)
+      const icon = this.getIcon(weather.icon)
       return (
         <div>
           <RefreshWeather/>
@@ -78,23 +102,15 @@ class Weather extends Component {
               <h5>{location}</h5>
             </div>
             <div className="weather_row">
+              {icon}
               <h3 className="weather_title">{weather.temp + " \u00b0"}C</h3>
             </div>
             <div className="weather_row">
-              <ReactAnimatedWeather
-                icon={'CLEAR_DAY'}
-                color={'#646464'}
-                size={25}
-                animate={true}
-              />
-              <h6 className="weather_title">{this.parseDescription(weather.description)}</h6>
-            </div>
-            <div className="weather_row">
-              <img src={sunrise_icon} alt="" className="weather_icon"/>
+              <img src={sunrise_icon} alt="" className="sunrise_icon"/>
               <h6 className="weather_title"><Moment format="HH:mm" unix>{weather.sunrise}</Moment></h6>
             </div>
             <div className="weather_row">
-              <img src={sunset_icon} alt="" className="weather_icon"/>
+              <img src={sunset_icon} alt="" className="sunrise_icon"/>
               <h6 className="weather_title"><Moment format="HH:mm" unix>{weather.sunset}</Moment></h6>
             </div>
           </div>
