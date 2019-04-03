14c5eadb785d39f98594598ea7924bc0c84b7510
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-04-03 08:41:39 +0200

    Wrap isAuth in a promise and proper error checking

--- client/src/calendar/calendar.js
+++ client/src/calendar/calendar.js
@@ -19,7 +19,11 @@ class Calendar extends Component {
     }
 
     componentDidMount() {
-      this.isAuth()
+      this.isAuth().then(() => {
+        if (this.state.authorized) {
+          this.handleRefresh()
+        }
+      })
     }
 
     handleRefresh() {
@@ -48,19 +52,26 @@ class Calendar extends Component {
           })
         }
       })
+      .catch(err => {
+        console.log(err)
+      })
     }
 
     isAuth() {
-      fetch('/calendar_authenticated' + localStorage.getItem('email'))
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        if (data.authorized === 'NOT_AUTHORIZED') {
-          this.setState({ authorized: false })
-        } else {
-          this.setState({ authorized: true })
-        }
+      return new Promise(resolve => {
+        fetch('/calendar_authenticated' + localStorage.getItem('email'))
+        .then(response => {
+          return response.json()
+        })
+        .then(data => {
+          if (data.authorized === 'NOT_AUTHORIZED') {
+            this.setState({ authorized: false })
+            resolve('NOT_AUTHORIZED')
+          } else {
+            this.setState({ authorized: true })
+            resolve('AUTHORIZED')
+          }
+        })
       })
     }
 
@@ -71,7 +82,7 @@ class Calendar extends Component {
 
     let events = []
     const calendar_events = localStorage.getItem('calendar_events')
-    if (typeof calendar_events !== 'undefined') {
+    if (calendar_events !== 'undefined') {
       events = JSON.parse(calendar_events)
     }
 
