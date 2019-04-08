fd97a5bf3244009f876eef624117a07d767b2768
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-04-08 09:22:00 +0200

    Set current username after login

--- client/src/authentication/login.js
+++ client/src/authentication/login.js
@@ -47,6 +47,7 @@ class Login extends Component {
             .then(response => {
               return response.json()})
             .then(data => {
+              localStorage.setItem('user_name', data.name)
               localStorage.setItem('user_station_name', data.station_name)
               localStorage.setItem('user_station_id', data.station_id)
               localStorage.setItem('user_bus', data.bus)
