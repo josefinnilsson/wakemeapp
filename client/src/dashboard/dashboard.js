f1ff5d82b3426b5526815992a9d01a09ede2896c
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-04-08 09:22:21 +0200

    Show greeting on dashboard

--- client/src/dashboard/dashboard.js
+++ client/src/dashboard/dashboard.js
@@ -17,12 +17,26 @@ class Dashboard extends Component {
     document.body.style.backgroundImage = `url(${url})`
   }
 
+  getGreeting() {
+    const hour = new Date().getHours()
+    if (hour > 5 && hour < 12)
+      return "Good Morning"
+    if (hour >= 12 && hour <= 17)
+      return "Good Afternoon"
+    else
+      return "Good Night"
+
+  }
+
   render() {
     let update = false
     if (this.props.location.pathname === '/cal')
       update = true
+    const greeting = this.getGreeting()
+    const name = localStorage.getItem('user_name')
     return (
       <div className="dashboard_wrapper">
+        <h2>{greeting} {name}!</h2>
         <div className="container">
           <div className="row">
             <div className="col-md-6">
