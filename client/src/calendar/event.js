9afb50db7bf236f55fe5c2dad7b2b4057f7f99cc
Josefin Nilsson <josefin.l.nilsson@gmail.com>
2019-04-11 16:44:06 +0200

    Lint fixes

--- client/src/calendar/event.js
+++ client/src/calendar/event.js
@@ -24,27 +24,28 @@ class Event extends Component {
 		}
 	}
 
-  render() {
-    const event = <h6 className="event_time">{this.props.start !== undefined && this.formatTime(this.props.start)} - {this.props.start !== undefined && this.formatTime(this.props.end)}</h6>
-    const longEvent = <h6 className="event_time">{this.props.start && 'All day'}</h6>
-    return (
-      <div className="event_item_wrapper">
-        <a href={this.props.link} rel="noopener noreferrer" target="_blank">
-          <h5 className="event_title">{this.props.summary}</h5>
-          {this.props.longEvent === true ? longEvent : event}
-          {this.props.location !== 'NO_LOCATION' && <h6 className="event_location">{this.formatLocation(this.props.location)}</h6>}
-        </a>
-      </div>
-    )
-  }
+	render() {
+		const event = <h6 className="event_time">{this.props.start !== undefined && this.formatTime(this.props.start)} - {this.props.start !== undefined && this.formatTime(this.props.end)}</h6>
+		const longEvent = <h6 className="event_time">{this.props.start && 'All day'}</h6>
+		return (
+			<div className="event_item_wrapper">
+				<a href={this.props.link} rel="noopener noreferrer" target="_blank">
+					<h5 className="event_title">{this.props.summary}</h5>
+					{this.props.longEvent === true ? longEvent : event}
+					{this.props.location !== 'NO_LOCATION' && <h6 className="event_location">{this.formatLocation(this.props.location)}</h6>}
+				</a>
+			</div>
+		)
+	}
 }
 
 Event.propTypes = {
-  link: PropTypes.string,
-  summary: PropTypes.string,
-  start: PropTypes.string,
-  end: PropTypes.string,
-  location: PropTypes.string,
+	link: PropTypes.string,
+	summary: PropTypes.string,
+	start: PropTypes.string,
+	end: PropTypes.string,
+	location: PropTypes.string,
+	longEvent: PropTypes.string,
 }
 
 export default Event
